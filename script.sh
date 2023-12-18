e079ca2dd17143b8d4c1184b91c8c26c145f6a03
Tomas Mikenda <tomas.mikenda@tietoevry.com>
2023-12-19 00:44:27 +0100

    dbg

--- script.sh
+++ script.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/sh
 echo "begin script"
+
 echo "begin script" > /tmp/log.txt
+
 export JFROG_CLI_OFFER_CONFIG=false
+
 export JFROG_CLI_HOME_DIR=/etc/jfrog
+
 echo "end script"
+
 ls -la /home >> /tmp/log.txt
-whoami >> /tmp/log.txt
+
+
+id >> /tmp/log.txt
+
 date >> /tmp/log.txt
-
-
-# https://raw.githubusercontent.com/let-thomas/adols/main/script.sh
-# az vmss extension set --vmss-name vmss-legacy-deploy --resource-group rg-agents --name CustomScript --version 2.0 --publisher Microsoft.Azure.Extensions --settings ./vmsse_script-config.json
-
-  
-# https://learn.microsoft.com/en-us/azure/virtual-machines/extensions/custom-script-linux
-
-# https://dev.azure.com/EnergyUtilities/7140623a-3172-4800-b830-a9188f3a523c/_apis/git/repositories/f92105a2-4e03-487f-80b8-23ef3977a501/items?path=/main.md&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0&download=true
-
-
-
