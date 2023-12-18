e8983ea5000dda171a5d7daaaf08ef01dfe25df4
Tomas Mikenda <tomas.mikenda@tietoevry.com>
2023-12-19 00:37:18 +0100

    qwe

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
+echo "begin script"
+echo "begin script" > /tmp/log.txt
 export JFROG_CLI_OFFER_CONFIG=false
 export JFROG_CLI_HOME_DIR=/etc/jfrog
 echo "end script"
-whoami > /tmp/log.txt
+ls -la /home >> /tmp/log.txt
+whoami >> /tmp/log.txt
 date >> /tmp/log.txt
 
 
@@ -11,7 +14,7 @@ date >> /tmp/log.txt
   
 # https://learn.microsoft.com/en-us/azure/virtual-machines/extensions/custom-script-linux
 
-#https://dev.azure.com/EnergyUtilities/7140623a-3172-4800-b830-a9188f3a523c/_apis/git/repositories/f92105a2-4e03-487f-80b8-23ef3977a501/items?path=/main.md&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0&download=true
+# https://dev.azure.com/EnergyUtilities/7140623a-3172-4800-b830-a9188f3a523c/_apis/git/repositories/f92105a2-4e03-487f-80b8-23ef3977a501/items?path=/main.md&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0&download=true
 
 
 
