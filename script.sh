325bef5462d23a41635f944b553504f0902bc0aa
Tomas Mikenda <tomas.mikenda@tietoevry.com>
2023-12-19 01:06:45 +0100

    set vars

--- script.sh
+++ script.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/sh
 echo "begin script"
 
-echo "begin script" > /tmp/log.txt
+date -Ins > /tmp/log.txt
+echo "begin script" >> /tmp/log.txt
 
-export JFROG_CLI_OFFER_CONFIG=false
+if [ -f /agent/.env ]; then
+	echo jo
+	echo "JFROG_CLI_OFFER_CONFIG=false" >> /agent/.env
+	echo "JFROG_CLI_HOME_DIR=/etc/jfrog" >> /agent/.env
+	echo "ansible=true" >> /agent/.env
+else
+	echo neni file /agent/.env
+fi
 
-export JFROG_CLI_HOME_DIR=/etc/jfrog
-
-echo "end script"
 
 ls -la /home >> /tmp/log.txt
 
 
 id >> /tmp/log.txt
 
-date >> /tmp/log.txt
+date -Ins >> /tmp/log.txt
+echo "end script"
+date -Ins
