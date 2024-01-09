31301498d94ab87eae718422b3f6e5dd96405325
Tomas Mikenda <tomas.mikenda@tietoevry.com>
2024-01-09 12:58:39 +0100

    install jre

--- script.sh
+++ script.sh
@@ -22,5 +22,22 @@ ls -la /home >> /tmp/log.txt
 id >> /tmp/log.txt
 
 date -Ins >> /tmp/log.txt
+apt-get install -y openjdk-11-jre >> /tmp/log.txt
 echo "end script"
 date -Ins
+
+# openjdk-8-jdk
+# openjdk-8-jre
+# openjdk-11-jdk
+# openjdk-11-jre
+# openjdk-17-jdk
+# openjdk-17-jre
+# openjdk-18-jdk
+# openjdk-18-jre
+# openjdk-19-jdk
+# openjdk-19-jre
+# openjdk-21-jdk
+# openjdk-21-jre
+# msopenjdk-11
+# msopenjdk-17
+# msopenjdk-21
